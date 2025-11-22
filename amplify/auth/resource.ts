28a4f0f651e62fdcc4c7993d5ae7d04b46eb6f0a
Mkpoikana Udoekong <mkpoikana@example.com>
2025-11-22 19:18:14 +0100

    Configure Google OAuth + Email login with Cognito Hosted UI

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,27 +1,21 @@
-// amplify/auth/resource.ts
-import { Stack } from "aws-cdk-lib";
-import { Auth } from "@aws-amplify/auth-construct";
-
-// Use the stack from backend.ts if you want shared stack
-import { stack } from "../backend";
-
-// Create the Auth construct
-export const auth = new Auth(stack, "Auth", {
-  userPoolClientProps: {
-    oAuth: {
-      flows: {
-        authorizationCodeGrant: true, // enables OAuth code grant
+export const auth = defineAuth({
+  loginWith: {
+    email: true, // allow email/password login
+    externalProviders: {
+      google: {
+        clientId: "1018334428394-16srs953e06uc6qmd40rhnmegdlmlq8e.apps.googleusercontent.com",       
+        clientSecret: "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",
+        scopes: ["email", "openid", "profile"],   
+        redirectSignInUrl: "https://mkayapp-dev.auth.us-east-1.amazoncognito.com/oauth2/idpresponse/",  
+        redirectSignOutUrl: " https://mkayapp-dev.auth.us-east-1.amazoncognito.com/logout/", 
       },
-      scopes: ["email", "openid", "profile"],
-      callbackUrls: ["http://localhost:3000/"], // update as needed
-      logoutUrls: ["http://localhost:3000/"],
-      domainPrefix: "myapp", // replace with a unique prefix
     },
   },
-  userAttributes: {
-    email: true,
-  },
 });
+
+
+
+
 
 
 
